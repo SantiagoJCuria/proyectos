60aff24cf1ac99e99323cf3c53b7f4e106b0b7b3
SbandAid <santiagocuria1711@gmail.com>
2023-03-03 16:02:19 -0300

    Enprolijamos un poco js

--- CV_responsive/js/script.js
+++ CV_responsive/js/script.js
@@ -1,15 +1,11 @@
 let anchoPantalla = window.innerWidth;
 let primeraColumna = document.getElementById("primera_columna");
 console.log(primeraColumna);
-let botonRevelador = document.getElementById("boton_revelador");
-console.log(botonRevelador);
 
 function tamañoPrimeraColumna(ancho) {
     if (ancho <= 320) {
-        // Si la pantalla es más grande que 768px, establecer un ancho más grande
         primeraColumna.style.width = "80%";
       } else if (ancho <= 420) {
-        // Si la pantalla es más pequeña que 768px, establecer un ancho más pequeño
         primeraColumna.style.width = "70%";
       } else if (ancho <= 560){
         primeraColumna.style.width = "60%";
