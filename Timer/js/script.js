c46f0e1aeccc6c253af2d44d6913f2d08a4d9822
SbandAid <santiagocuria1711@gmail.com>
2023-03-05 16:32:43 -0300

    Pequeño cambio

--- Timer/js/script.js
+++ Timer/js/script.js
@@ -2,14 +2,14 @@ let botonPlay = document.querySelector("#botonPlay");
 let botonPause = document.querySelector("#botonPause");
 let botonStop = document.querySelector("#botonStop");
 let activado = false;
-function tiempoAumento() {
-    let tiempo = document.querySelector("p");
-    let tiempoValor = parseInt(tiempo.innerHTML);
-    let seg = tiempoValor + 1;
-    tiempo.innerHTML = seg;
-}
+let tiempo = document.querySelector("p");
 function encender() {
-     actualizacion = setInterval(tiempoAumento, 1000); 
+     actualizacion = setInterval(() => {
+        let tiempoValor = parseFloat(tiempo.innerHTML);
+        let segundos = tiempoValor + 1;
+        tiempo.innerHTML = segundos ;
+    }, 1000); 
+
 }
 function apagar() {
     clearInterval(actualizacion);
@@ -25,21 +25,3 @@ botonPause.addEventListener("click", ()=>{
     botonPause.style.display= "none";
     apagar();
 })
-
-function convertirsegundos(segundos) {
-    // Convertir milisegundos a segundos
-    let minutos = Math.floor((segundos % 3600) / 60);
-    
-    // Calcular horas, minutos y segundos
-    let horas = Math.floor(segundos / 3600);
-    let segundosRestantes = segundos % 60;
-  
-    // Crear objeto con las partes del tiempo
-    let tiempoFormato = {
-      horas: horas,
-      minutos: minutos,
-      segundos: segundosRestantes
-    };
-  
-    return tiempoFormato.horas + ":" + tiempoFormato.minutos + ":" + tiempoFormato.segundos;
-  }
