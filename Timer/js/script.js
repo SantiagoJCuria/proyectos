735227dc58bfa120fb03d5b9a2b111a5e7f9582b
SbandAid <santiagocuria1711@gmail.com>
2023-03-05 15:56:46 -0300

    Problema al pasar los segundos a horas y minutos

--- Timer/js/script.js
+++ Timer/js/script.js
@@ -2,17 +2,18 @@ let botonPlay = document.querySelector("#botonPlay");
 let botonPause = document.querySelector("#botonPause");
 let botonStop = document.querySelector("#botonStop");
 let activado = false;
-
+function tiempoAumento() {
+    let tiempo = document.querySelector("p");
+    let tiempoValor = parseInt(tiempo.innerHTML);
+    let seg = tiempoValor + 1;
+    tiempo.innerHTML = seg;
+}
 function encender() {
-     actualizacion = setInterval(() => {
-        let tiempo = document.querySelector("p");
-        let tiempoValor = parseFloat(tiempo.innerHTML);
-        let milisegundos = tiempoValor + 1; 
-        tiempo.innerHTML = milisegundos + 1;
-    }, 1); 
+     actualizacion = setInterval(tiempoAumento, 1000); 
 }
 function apagar() {
     clearInterval(actualizacion);
+
 }
 botonPlay.addEventListener("click", ()=>{
     botonPlay.style.display = "none";
@@ -24,3 +25,21 @@ botonPause.addEventListener("click", ()=>{
     botonPause.style.display= "none";
     apagar();
 })
+
+function convertirsegundos(segundos) {
+    // Convertir milisegundos a segundos
+    let minutos = Math.floor((segundos % 3600) / 60);
+    
+    // Calcular horas, minutos y segundos
+    let horas = Math.floor(segundos / 3600);
+    let segundosRestantes = segundos % 60;
+  
+    // Crear objeto con las partes del tiempo
+    let tiempoFormato = {
+      horas: horas,
+      minutos: minutos,
+      segundos: segundosRestantes
+    };
+  
+    return tiempoFormato.horas + ":" + tiempoFormato.minutos + ":" + tiempoFormato.segundos;
+  }
